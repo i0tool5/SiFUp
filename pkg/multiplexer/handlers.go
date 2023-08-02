1f5e17683fde8cb8cf08d61ca7d1f5c32e0e6ed4
i0tool5 <sebe.p@yandex.ru>
2023-08-02 18:06:49 +0300

    Fixed large file upload issue
    When uploading large file (more than available space in /tmp on linux)
    the application will crash with error:
    "write /tmp/multipart-3034808061: no space left on device."
    Therefore, it is necessary to process the data in chunks and write
    them to a file.

--- pkg/multiplexer/handlers.go
+++ pkg/multiplexer/handlers.go
@@ -1,14 +1,21 @@
 package multiplexer
 
 import (
+	"bufio"
+	"bytes"
+	"errors"
 	"fmt"
 	"io"
-	"log"
+	"mime/multipart"
 	"net/http"
+	"os"
+	"sync"
 
 	"github.com/i0tool5/simpleuploader/pkg/helpers"
 	"github.com/i0tool5/simpleuploader/pkg/templates"
 )
+
+var chunkSize = 512
 
 type Handlers struct {
 	saveToDir string
@@ -22,6 +29,16 @@ func NewHandlers(saveToDir string) (h *Handlers) {
 	h.templates = make(map[string]string)
 
 	return
+}
+
+var bufPool = sync.Pool{
+	New: func() any {
+		// The Pool's New function should generally only return pointer
+		// types, since a pointer can be put into the return interface
+		// value without an allocation:
+
+		return bytes.NewBuffer(make([]byte, chunkSize))
+	},
 }
 
 // PrepareTemplates pre-creates template instances
@@ -44,43 +61,74 @@ func (h *Handlers) PrepareTemplates() (err error) {
 	return
 }
 
+// HandleMain is responsible for handling requests to the main page.
 func (h *Handlers) HandleMain(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, h.templates["form"])
 }
 
+// HandleFiles is responsible for handling incoming files requests
 func (h *Handlers) HandleFiles(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseMultipartForm(10_000)
+	multiPartReader, err := r.MultipartReader()
 	if err != nil {
 		helpers.WrapBoth(w, err)
 		return
 	}
 
-	files := r.MultipartForm.File
-	if len(files["file"]) == 0 {
-		log.Println("[!] No files to save.")
-		w.WriteHeader(http.StatusBadRequest)
-		helpers.WrapHttp(w)
-		return
-	}
+	buff := bufPool.Get().(*bytes.Buffer)
+	defer bufPool.Put(buff)
+	defer buff.Reset()
 
-	for _, file := range files["file"] {
-		cf, err := file.Open()
-		if err != nil {
-			helpers.WrapBoth(w, err)
-			return
-		}
-		defer cf.Close()
-		fileContent, _ := io.ReadAll(cf)
-		err = helpers.CreateFile(h.saveToDir, file.Filename, fileContent)
-		if err != nil {
-			log.Printf("[!] Error occured creating file %s\n", err)
-		}
+	err = handleMultiPartReader(multiPartReader, buff, h.saveToDir)
+	if err != nil {
+		helpers.WrapBoth(w, err)
 	}
 
 	fmt.Fprint(w, h.templates["success"])
 }
 
+// HandleFonts is responsible for handling font requests
 func (h *Handlers) HandleFonts(w http.ResponseWriter, r *http.Request) {
 	font := h.templates["genos"]
 	fmt.Fprint(w, font)
+}
+
+func handleMultiPartReader(mpReader *multipart.Reader, buff *bytes.Buffer, saveDir string) error {
+	for {
+		nextPart, err := mpReader.NextPart()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			return err
+		}
+
+		wfile, err := os.Create(saveDir + nextPart.FileName())
+		if err != nil {
+			return err
+		}
+		defer wfile.Close()
+
+		bufRead := bufio.NewReader(nextPart)
+		bufWriter := bufio.NewWriter(wfile)
+
+		for {
+			dat := buff.Bytes()
+			n, err := bufRead.Read(dat)
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					bufWriter.Flush()
+					break
+				}
+				return err
+			}
+
+			_, err = bufWriter.Write(dat[:n])
+			if err != nil {
+				return err
+			}
+			bufWriter.Flush()
+		}
+	}
+
+	return nil
 }
